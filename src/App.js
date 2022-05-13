73bc688bc9266c95fba3c4fd0eb16055d5aa98d9
Oladimeji Abdrahman Adebayo <67334984+rahmlad-aramide@users.noreply.github.com>
2022-05-13 06:28:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import Nav from "./components/Nav";
+import QuizScreen from "./components/QuizScreen";
+import JoinScreen from "./components/JoinScreen";
+import Footer from "./components/Footer"
+
+const App = () => {
+	const [isQuizStarted, setIsQuizStarted] = React.useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+      <Nav />
+	  <div className="quiz-container flex justify-center items-center h-[85vh]">
+		<div className="border-2 relative bg-white shadow lg:w-[50vw] w-[95%]">
+			{isQuizStarted? (
+				<QuizScreen retry={()=>setIsQuizStarted(false)}/>	
+			): (
+				<JoinScreen start={()=>setIsQuizStarted(true)}/>
+			)}
+		</div>
+	  </div>
+      <Footer />
+    </>
+    
   );
-}
+};
 
 export default App;
